d4a2a17f1483d20f5416e7626331c8fdbfa17822
Agnel Waghela <agnelwaghela@gmail.com>
2025-10-20 14:58:58 +0530

    feat: implement step 4 - support different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,6 +5,14 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).reduce(:+)
+    delimiter = /,|\n/
+
+    if numbers.start_with?('//')
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..])
+      numbers = parts[1]
+    end
+
+    numbers.split(delimiter).map(&:to_i).reduce(:+)
   end
 end
