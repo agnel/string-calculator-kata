57022b00675ca543787d0034cf4b177bc36255e5
Agnel Waghela <agnelwaghela@gmail.com>
2025-10-20 14:39:04 +0530

    feat: implement step 3 - handling newline delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,6 +5,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(',').map(&:to_i).reduce(:+)
+    numbers.split(/,|\n/).map(&:to_i).reduce(:+)
   end
 end
