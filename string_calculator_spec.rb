56a9b16c396d1eb4284ec63d00f162ed21e4450a
Agnel Waghela <agnelwaghela@gmail.com>
2025-10-20 14:34:58 +0530

    test: step 3 - handles newlines as delimiters

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -22,6 +22,10 @@ describe StringCalculator do
     it 'sums unknown amount of comma separated numbers' do
       expect(calculator.add('1,2,3,4,5')).to eq(15)
     end
+
+    it 'handles newlines as delimiters' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
 # rubocop:enable Metrics/BlockLength
